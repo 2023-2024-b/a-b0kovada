7dbd00915c912a84917602252683b3727b54107a
b0kovada <kovacshaziadam@gmail.com>
2024-11-26 10:35:16 +0100

    kommit

--- csarp-back-00-01-01-backend-study/csarp-back-00-01-01-backend-study/Contexts/KretaContext.cs
+++ csarp-back-00-01-01-backend-study/csarp-back-00-01-01-backend-study/Contexts/KretaContext.cs
@@ -4,6 +4,8 @@ namespace csarp_back_00_01_01_backend_study.Contexts
 {
     public class KretaContext : DbContext
     {
-
+        public KretaContext(DbContextOptions options) : base(options)
+        {
+        }
     }
 }
--- csarp-back-00-01-01-backend-study/csarp-back-00-01-01-backend-study/Contexts/KretaInMemoryContext.cs
+++ csarp-back-00-01-01-backend-study/csarp-back-00-01-01-backend-study/Contexts/KretaInMemoryContext.cs
@@ -4,6 +4,10 @@ namespace csarp_back_00_01_01_backend_study.Contexts
 {
     public class KretaInMemoryContext : KretaContext
     {
+        public KretaInMemoryContext(DbContextOptions<KretaInMemoryContext> options):base(options)
+        {
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
